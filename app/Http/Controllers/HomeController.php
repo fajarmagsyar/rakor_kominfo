3769617b376618780d0ea27be3afab3ce5d90221
Fajar Magsyar <84609241+fajarmagsyar@users.noreply.github.com>
2022-06-02 12:27:18 +0800

    bootstrap template addition for user

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,9 +6,10 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('home.index', [
-            'page' => 'Home',
+            'pageTitle' => 'Home',
         ]);
     }
 }
