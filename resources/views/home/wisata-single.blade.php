f0eb1a94c27565b57e0a7e71ea04b25b121d2a58
Shiniciro12 <febykromen@gmail.com>
2022-06-04 16:23:37 +0700

    user wisata, hotel dan restoran

---
+++ resources/views/home/wisata-single.blade.php
@@ -0,0 +1,38 @@
+@extends('home.layouts.main')
+@section('isi')
+    <br>
+    <br>
+
+    <main id="main">
+       
+        <!-- ======= About Section ======= -->
+        <section id="about" class="about">
+            <div class="container" data-aos="fade-up">
+                <div class="row g-4 g-lg-5" data-aos="fade-up" data-aos-delay="200">
+
+                    <div class="col-lg-5">
+                        <div class="about-img">
+                            <img src="/assets/img/wisata/krissstal.jpg" class="img-fluid" alt="">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-7">
+                        <h3 class="pt-0 pt-lg-5">Goa Kristal</h3>
+                        <h6 class="fst-italic">
+                            <i class="bi bi-geo-alt-fill" size="8px" style="color:#B22222 ;"></i> Bolok, Kec. Kupang Bar., Kabupaten Kupang, Nusa Tenggara Tim.
+                        </h6>
+                        <br>
+                        <p style="text-align: justify;">Letak Gua Kristal berada di dalam celah Goa Karst. Di sini kamu akan melihat pemandangan dinding-dinding goa yang dipenuhi bebatuan. 
+                            Yang menjadi daya tarik utama tentu air di dasar kolam yang begitu jernih dan menampakkan bebatuan di dalamnya bak kristal.</p>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </section><!-- End About Section -->
+
+
+    </main><!-- End #main -->
+@endsection
